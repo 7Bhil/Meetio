a6ef999bd7a022a48d181b35ea4def91f77d62a4
7Bhil <7bhilal.chitou7@gmail.com>
2026-02-08 23:55:36 +0100

    Add build script for Flutter

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Dossier d'installation de Flutter
+FLUTTER_PATH="$HOME/flutter"
+
+# Cloner Flutter si il n'existe pas
+if [ ! -d "$FLUTTER_PATH" ]; then
+    echo "📥 Téléchargement du SDK Flutter..."
+    git clone https://github.com/flutter/flutter.git -b stable --depth 1 "$FLUTTER_PATH"
+fi
+
+# Ajouter Flutter au PATH
+export PATH="$PATH:$FLUTTER_PATH/bin"
+
+# Désactiver les analytics pour gagner du temps
+flutter config --no-analytics
+
+# Télécharger les outils de build web
+flutter precache --web
+
+# Builder le projet
+echo "🏗️ Début du build Flutter Web..."
+flutter build web --release
